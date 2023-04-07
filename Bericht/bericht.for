a35bac99703ed8897c56aae1d82816acc79d1de1
Alena Sutiagina <Alena.Sutiagina@cas.de>
2023-04-07 21:15:23 +0200

    Add the analysis of the commands koncept

--- Bericht/bericht.for
+++ Bericht/bericht.for
@@ -1,12 +1,6 @@
-\defcounter {refsection}{0}\relax 
 \contentsline {eq}{(\ref {eq-tf}) Wert der Worthäufigkeit im Dokument}{18}{equation.2.3.1}%
-\defcounter {refsection}{0}\relax 
 \contentsline {eq}{(\ref {eq-idf}) Wert der inversen Dokumenthäufigkeit}{18}{equation.2.3.2}%
-\defcounter {refsection}{0}\relax 
 \contentsline {eq}{(\ref {eq-tfidf}) TF-IDF-Wert}{18}{equation.2.3.3}%
-\defcounter {refsection}{0}\relax 
 \contentsline {eq}{(\ref {eq-swz}) Singularitätwertzerlegung}{23}{equation.2.3.4}%
-\defcounter {refsection}{0}\relax 
 \contentsline {eq}{(\ref {eq-trunkierte-swz}) Trunkierte SWZ-Matrix}{25}{equation.2.3.5}%
-\defcounter {refsection}{0}\relax 
 \contentsline {eq}{(\ref {eq-entropie}) Entropie H}{35}{equation.2.3.6}%
