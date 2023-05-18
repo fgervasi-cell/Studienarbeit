32b659bd467c3a06108389f8e7a34fd4bce8dd86
fgervasi-cell <gervasi.felix@student.dhbw-karlsruhe.de>
2023-05-18 14:03:38 +0200

    Stuff.

--- arduino/serial_com_tut/serial_com_tut.ino
+++ arduino/serial_com_tut/serial_com_tut.ino
@@ -26,16 +26,5 @@ void loop() {
   if (Serial.available() > 0) {
     String data = Serial.readStringUntil('\n');
     Serial.println("Received " + data);
-    analogWrite(pumpe1, (getValueAt(data, 0) * 0.5 + 50) * 2.55);
-    analogWrite(pumpe2, (getValueAt(data, 1) * 0.5 + 50) * 2.55);
-    analogWrite(pumpe3, (getValueAt(data, 2) * 0.5 + 50) * 2.55);
-    analogWrite(pumpe4, (getValueAt(data, 3) * 0.5 + 50) * 2.55);
-    delay(3000);
-  } else {
-    analogWrite(pumpe1, 0);
-    analogWrite(pumpe2, 0);
-    analogWrite(pumpe3, 0);
-    analogWrite(pumpe4, 0);
-    analogWrite(pumpe5, 0);
   }
 }
