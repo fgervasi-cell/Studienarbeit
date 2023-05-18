7a7dc831762246c08b0ee9e142302e8e86c99eec
Alena Sutiagina <Alena.Sutiagina@cas.de>
2023-05-18 18:12:05 +0200

    Add the part of what the maschine can do

--- Bericht/bericht.for
+++
@@ -1,9 +0,0 @@
-\contentsline {eq}{(\ref {eu-norm}) Euklidische Norm}{27}{equation.2.3.1}%
-\contentsline {eq}{(\ref {eu-skal-prod}) Euklidisches Skalarprodukt}{28}{equation.2.3.2}%
-\contentsline {eq}{(\ref {och-coef}) Ochiai-Koeffizient}{28}{equation.2.3.3}%
-\contentsline {eq}{(\ref {eq-tf}) Wert der Worthäufigkeit im Dokument}{30}{equation.2.3.4}%
-\contentsline {eq}{(\ref {eq-idf}) Wert der inversen Dokumenthäufigkeit}{30}{equation.2.3.5}%
-\contentsline {eq}{(\ref {eq-tfidf}) TF-IDF-Wert}{30}{equation.2.3.6}%
-\contentsline {eq}{(\ref {eq-swz}) Singularitätwertzerlegung}{35}{equation.2.3.7}%
-\contentsline {eq}{(\ref {eq-trunkierte-swz}) Trunkierte SWZ-Matrix}{37}{equation.2.3.8}%
-\contentsline {eq}{(\ref {eq-entropie}) Entropie H}{47}{equation.2.3.9}%
