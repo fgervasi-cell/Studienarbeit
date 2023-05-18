e410009589d8d921da37615360d3594f38ae39f4
fgervasi-cell <gervasi.felix@student.dhbw-karlsruhe.de>
2023-05-18 19:47:20 +0200

    Finished.

---
+++ Bericht/bericht.for
@@ -0,0 +1,18 @@
+\defcounter {refsection}{0}\relax 
+\contentsline {eq}{(\ref {eu-norm}) Euklidische Norm}{29}{equation.2.3.1}%
+\defcounter {refsection}{0}\relax 
+\contentsline {eq}{(\ref {eu-skal-prod}) Euklidisches Skalarprodukt}{30}{equation.2.3.2}%
+\defcounter {refsection}{0}\relax 
+\contentsline {eq}{(\ref {och-coef}) Ochiai-Koeffizient}{30}{equation.2.3.3}%
+\defcounter {refsection}{0}\relax 
+\contentsline {eq}{(\ref {eq-tf}) Wert der Worthäufigkeit im Dokument}{32}{equation.2.3.4}%
+\defcounter {refsection}{0}\relax 
+\contentsline {eq}{(\ref {eq-idf}) Wert der inversen Dokumenthäufigkeit}{32}{equation.2.3.5}%
+\defcounter {refsection}{0}\relax 
+\contentsline {eq}{(\ref {eq-tfidf}) TF-IDF-Wert}{32}{equation.2.3.6}%
+\defcounter {refsection}{0}\relax 
+\contentsline {eq}{(\ref {eq-swz}) Singularitätwertzerlegung}{39}{equation.2.3.7}%
+\defcounter {refsection}{0}\relax 
+\contentsline {eq}{(\ref {eq-trunkierte-swz}) Trunkierte SWZ-Matrix}{41}{equation.2.3.8}%
+\defcounter {refsection}{0}\relax 
+\contentsline {eq}{(\ref {eq-entropie}) Entropie H}{52}{equation.2.3.9}%
